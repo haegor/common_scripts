c0ba7c007787cefcce9cd654cd10a21add4bcf31
Yegor Chepurin <haegor+git@gmail.com>
2023-10-03 13:32:24 +0300

    Добавлен ./Restore/mysql_db_restore.sh

---
+++ Restore/mysql_db_restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Исполняет sql-скрипт применительно к базе в mysql
+# Подразумевается использование в целях автоматического восстановления базы
+#
+
+username=$1
+password=$2
+db_name=$3
+db_sql=$4
+
+mysql -u ${username} --database=${db_name} -p"${password}" < ${db_sql}
