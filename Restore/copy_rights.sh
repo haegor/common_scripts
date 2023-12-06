8ab0248d44b0ddf9a080df7067b1b4100eb5b553
Yegor Chepurin <haegor@gmail.com>
2023-12-06 22:57:03 +0300

    Добавлен help; обработка ошибок передалана на массивы; внедрены функции.

--- Restore/copy_rights.sh
+++ Restore/copy_rights.sh
@@ -1,61 +1,129 @@
 #!/bin/bash
-# author: haegor
-# date: 2020-07-07
-# DESCRIPTION:
-# Скрипт предназначен для копирования прав и владельцев файлов
-# с целью переноса на другую систему или бэкапа.
-# TODO когда указываешь restore, то нужно вводить лишний параметр - место восстановления. Надо чёт с этим сделать. Да и вообще красоту навести в управлении скриптом
+#
+# Скрипт предназначен для создания свода владельцев файлов и их прав на файлы
+# с целью применения этих настроек по месту требования.
+#
+# Когда указываешь restore, то нужно вводить "лишний" параметр, который вовсе
+# не лишний. Он используется в случаях, когда используется chroot, lxc или 
+# удалённая ФС, смонтированная через sshfs/smb/nfs.
+#
+# 2020-2023 (c) haegor
+#
 
-if [ -n "$1" ]; then
-        mode=$1
+f_save_settings () {
+  chosen_path="$1"
+  chosen_file="$2"
+
+  find "${chosen_path}" -exec ls -lad --full-time '{}' \; |  grep -v "^l" | while read record
+  do
+     owner=$(echo ${record} | cut -f3 -d' ')
+     group=$(echo ${record} | cut -f4 -d' ')
+     file=$(echo ${record} | cut -f9- -d' ')
+     rights=$(stat --printf=%a "${file}" 2>/dev/null)
+
+     echo "{rights} ${owner} ${group} ${file}" >> "${chosen_file}"
+  done
+}
+
+f_restore_settings () {
+  chosen_path="$1"
+  chosen_file="$2"
+
+  cat "${chosen_file}" | while read record
+  do
+     rights=$(echo ${record} | cut -f1 -d' ')
+     owner=$(echo ${record} | cut -f2 -d' ')
+     group=$(echo ${record} | cut -f3 -d' ')
+     file=$(echo ${record} | cut -f4- -d' ')
+
+     chown ${owner}:${group} "${chosen_path}"/"${file}"
+     chmod ${rights} "${chosen_path}"/"${file}"
+
+# TODO: Вот над этим следует серьёзно подумать.
+#
+#     if [ "$chosen_path" == '/' ]
+#     then    
+#       chown ${owner}:${group} "${file}"
+#       chmod ${rights} "${file}"
+#     elif [ "$chosen_path" == './' ]
+#     then 
+#       chown ${owner}:${group} ".${file}"
+#       chmod ${rights} ".${file}"
+#     else
+#       chown ${owner}:${group} "${chosen_path}"/"${file}"
+#       chmod ${rights} "${chosen_path}"/"${file}"
+#     fi
+  done
+}
+
+
+if [ "$1" ] 
+then
+    mode="$1"
+else 
+    reason[${#reason[@]}]="Недостаточно параметров. Не указан режим работы скрипта."
+    reason[${#reason[@]}]="Выберите из: save, restore."
+    reason[${#reason[@]}]=''
+    mode="help"
+fi
+
+if [ "$2" ] 
+then 
+    chosen_path="$2"
 else
-        echo "Not selected script mode. Please, choose one: save, restore."
-        echo "Common way to use utility is:"
-        echo "copy_rights (restore|save) [path] [filename]"
-        exit 0
+    reason[${#reason[@]}]="Недостаточно параметров."
+    reason[${#reason[@]}]="Укажите папку для обработки."
+    reason[${#reason[@]}]=''
+    mode="help"
 fi
 
-
-if [ -n "$2" ]; then
-        chosen_path=$2
+if [ "$3" ]
+then
+    chosen_file="$3"
 else
-        echo "Inseficcient paramaters"
-        echo "Common way to use utility is:"
-        echo "copy_rights (restore|save) [path] [filename]"
-        exit 0
+    reason[${#reason[@]}]="Недостаточно параметров."
+    reason[${#reason[@]}]="Укажите файл для сохранения/восстановления настроек."
+    reason[${#reason[@]}]=''
+    mode="help"
 fi
 
+case $mode in
+'save')				# Сохранение сведений о файлах
+  f_save_settings "${chosen_path}" "${chosen_file}"
+  echo "Операция сохранения завершена."
 
-if [ -n "$3" ]; then
-        chosen_file=$3
-else
-        echo "Inseficcient paramaters"
-        echo "Common way to use utility is:"
-        echo "copy_rights (restore|save) [path] [filename]"
-        exit 0
-fi
+;;
+'restore')			# Восстановление сведений о файлах
+  f_restore_settings "${chosen_path}" "${chosen_file}"
+  echo "Операция восстановления завершена."
+;;
+''|*|'--help'|'-h'|'help')	# Помощь. Мы тут.
+  echo
 
+  if [ ${#reason[*]} -eq 0 ]
+  then
+	  echo "Указан несуществующий режим работы скрипта."
+  fi
 
-if [ "$1" = 'save' ]; then
-        find ${chosen_path} -exec ls -lad --full-time '{}' \; |  grep -v "^l" | while read record
-        do
-           owner=$(echo ${record} | cut -f3 -d' ')
-           group=$(echo ${record} | cut -f4 -d' ')
-           file=$(echo ${record} | cut -f9- -d' ')
-           rights=$(stat --printf=%a "${file}" 2>/dev/null)
-           echo ${rights} ${owner} ${group} ${file} >> ${chosen_file}
-        done
-fi
+  for i in `seq 0 ${#reason[*]}`
+  do
+      echo "${reason[i]}"
+  done
 
-if [ "$1" = 'restore' ]; then
-        cat ${chosen_file} | while read record
-        do
-           rights=$(echo ${record} | cut -f1 -d' ')
-           owner=$(echo ${record} | cut -f2 -d' ')
-           group=$(echo ${record} | cut -f3 -d' ')
-           file=$(echo ${record} | cut -f4- -d' ')
+  echo "Параметры:"
+  echo "$0 (restore|save) <целевая папка> <результирующий файл>"
+  echo
+  echo "Режимы работы:"
+  grep -P "^\'[[:graph:]]*\'\)[[:space:]]*#?.*$" $0 | grep -v 'excluder'
+  echo
+  echo "Аргмуенты:"
+  echo "Первый - тип действия"
+  echo "Второй - папка для анализа/наложения настроек"
+  echo "Третий - результирующий файл"
+  echo
+  echo "При указании путей используйте кавычки"
+  echo
+  exit 0
+;;
+esac
 
-           chown ${owner}:${group} "${file}"
-           chmod ${rights} "${file}"
-        done
-fi
