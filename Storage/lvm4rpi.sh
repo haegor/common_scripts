b466c1c3359d46f476757b80efbb9d48cb0589cf
Yegor Chepurin <haegor@gmail.com>
2024-03-07 20:49:10 +0300

    Добавлен функционал, ручники и большая пачка остановов. Исправлены описания.

--- Storage/lvm4rpi.sh
+++ Storage/lvm4rpi.sh
@@ -13,9 +13,11 @@ exit 0
 
 # TODO Надо ещё сделать прописывание в /boot/firmware/config.txt
 # НО! Важно помнить, что:
-# Загрузка по имени /dev/mapper/storage-root - не работает.
-# Загрузка по uuid - не работает. Не хочет он рутить партицию из LVM.
-# При этом всё остальное спокойно подключает.
+# Загрузка по uuid - не работает. Вообще, даже без LVM.
+# Зато работает по имени блочного устройства и PARTUUID.
+# Последний выдаётся только primary партициям.
+# Но если подсовывать устройство дев-маппера (/dev/mapper/storage-root) для 
+# LVM, но и оно не сработает. При этом остальное спокойно подключает.
 
 #  А вообще, вроде как, лучше всего использовать sgdisk для gpt партиций
 parted='sudo parted'
@@ -79,7 +81,7 @@ case $1 in
   apt install xfsprogs lvm2
 ;;
 'mk-firmware')				# Создать 1 раздел, для firmware
-  $parted -a optimal $disk_name mkpart primary 0% 550MiB
+  $parted -a optimal $disk_name mkpart primary 0% 550MiB || exit 1
 #  sgdisk --new=1:0%:550MiB /dev/sda
   mkfs.vfat "${disk_name}1" \
     && echo "Раздел для прошивки создан." \
@@ -87,7 +89,7 @@ case $1 in
   $0 print
 ;;
 'mk-boot')				# Создать 2 раздел, для boot
-  $parted -a optimal $disk_name mkpart primary 550MiB 2050MiB
+  $parted -a optimal $disk_name mkpart primary 550MiB 2050MiB || exit 1
 #  sgdisk --new=2:550MiB:2050MiB /dev/sda
   mkfs.ext4 "${disk_name}2" \
     && echo "Загрузочный раздел создан." \
@@ -95,7 +97,7 @@ case $1 in
   $0 print
 ;;
 'mk-root')				# Создать 3, рутовую партицию
-  $parted -a optimal $disk_name mkpart primary 2050MiB 34050MiB
+  $parted -a optimal $disk_name mkpart primary 2050MiB 34050MiB || exit 1
 #  sgdisk --new=3:2050MiB:34050MiB /dev/sda
   mkfs.ext4 "${disk_name}3" \
     && echo "Корневой раздел создан." \
@@ -103,10 +105,10 @@ case $1 in
   $0 print
 ;;
 'mk-main')				# Создать 4 партицию, для PV + добавить в VG
-  $parted -a optimal $disk_name mkpart primary 34050MiB 1TB
+  $parted -a optimal $disk_name mkpart primary 34050MiB 1TB || exit 1
 #  sgdisk --new=4:34050MiB:1TiB /dev/sda
-  pvcreate "${disk_name}4"
-  vgcreate $lvm_vg_name "${disk_name}4"
+  pvcreate "${disk_name}4" || exit 1
+  vgcreate $lvm_vg_name "${disk_name}4" || exit 1
   echo "Раздел под LVM создан."
   $0 print
 ;;
@@ -129,39 +131,46 @@ case $1 in
 ;;
 'mk-containers')			# Создание одного волюма - "containers" на 20% места
   lvm_lv_name='containers'
-  lvcreate -L 20%FREE -n $lvm_lv_name $lvm_vg_name
+  lvcreate -l 20%FREE -n $lvm_lv_name $lvm_vg_name || exit 1
+  mkfs.xfs "/dev/mapper/${lvm_vg_name}-${lvm_lv_name}"
+;;
+'mk-volumes')
+  lvm_lv_name='volumes'
+  lvcreate -l 20%FREE -n $lvm_lv_name $lvm_vg_name || exit 1
   mkfs.xfs "/dev/mapper/${lvm_vg_name}-${lvm_lv_name}"
 ;;
 'mk-lv-fs')				# Создаёт FS на созданных LV
+  exit 0	# Ручник. Эта секция форматирует диски!
+
   for volume in $(ls /dev/mapper/${lvm_vg_name}-*)
   do
     lv=${volume#/dev/mapper/${lvm_vg_name}-}
 
-    [ "$lv" == "swap" ] && mkswap    $volume
-    [ "$lv" == "opt" ]  && mkfs.ext4 $volume
-    [ "$lv" == "var" ]  && mkfs.xfs  $volume
-    [ "$lv" == "home" ] && mkfs.xfs  $volume
+    [ "$lv" == "swap" ] && { mkswap    $volume || exit 1; }
+    [ "$lv" == "opt" ]  && { mkfs.ext4 $volume || exit 1; }
+    [ "$lv" == "var" ]  && { mkfs.xfs  $volume || exit 1; }
+    [ "$lv" == "home" ] && { mkfs.xfs  $volume || exit 1; }
   done
 ;;
 'migrate2nvme')				# Примонтировать, скопировать, подчистить root
   [ -d "/mnt/boot/firmware" ] && mkdir -p /mnt/boot/firmware \
     || { echo "Нельзя создать папку под будущую boot партицию."; exit 1; }
 
-  mount -t ext4 ${disk_name}2 /mnt/boot 2> /dev/null
-  mount -t vfat ${disk_name}1 /mnt/boot/firmware 2>/dev/null
-  mount -t ext4 ${disk_name}3 /mnt/root 2>/dev/null
+  mount -t ext4 ${disk_name}2 /mnt/boot 2> /dev/null || exit 1
+  mount -t vfat ${disk_name}1 /mnt/boot/firmware 2>/dev/null || exit 1
+  mount -t ext4 ${disk_name}3 /mnt/root 2>/dev/null || exit 1
 
   echo "Rsync of firmware"
-  rsync -arx /boot/firmware/ /mnt/boot/firmware/
+  rsync -arx /boot/firmware/ /mnt/boot/firmware/ || exit 1
 
   echo "Rsync of boot"
-  rsync -arx /boot/ /mnt/boot/
+  rsync -arx /boot/ /mnt/boot/ || exit 1
 
   echo "Rsync of root"
   rsync -arx \
     --exclude='/opt/*' --exclude='/home/*' \
     --exclude='/var/*' --exclude='/boot/*' \
-    / /mnt/root/
+    / /mnt/root/ || exit 1
 
   # Вычисляем список томов, создаём под них папки, монтируем и rsync-аем
   for volume in $(ls /dev/mapper/${lvm_vg_name}-*)
@@ -169,16 +178,16 @@ case $1 in
     lv=${volume#/dev/mapper/${lvm_vg_name}-}
     echo "LV: $lv"
 
-    [ -d "/mnt/$lv" ] && mkdir -p "/mnt/$lv"
-    mount $volume "/mnt/$lv" 2> /dev/null
+    [ -d "/mnt/$lv" ] && { mkdir -p "/mnt/$lv" || exit 1; }
+    mount $volume "/mnt/$lv" 2> /dev/null || exit 1
 
-    [ "$lv" == 'swap' ] && { continue; }
+    [ "$lv" == 'swap' ] && continue
 
     [ -d "/$lv" ] && rsync -arx "/$lv/" "/mnt/$lv/"
   done
 ;;
-'complete-del'|'complete-rm')		# e!xcluder "Секретный уровень". Удалит все три партиции с диска
-#  exit 0				# Ручник. ОПАСНО!!! Реально всё удалит. В клочья прям. По настоящему.
+'complete-del'|'complete-rm')		# excluder "Секретный уровень". Удалит все три партиции с диска
+  exit 0				# Ручник. ОПАСНО!!! Реально всё удалит. В клочья прям. По настоящему.
   umount ${disk_name}*
   umount /dev/mapper/${lvm_vg_name}-*
   $0 deactivate			# Деактивация LV
@@ -215,6 +224,10 @@ case $1 in
     [ "$lv" == "opt" ]  && { f_write2fstab "$uuid" '\t/opt	ext4	defaults,noatime	0 0' && continue; }
     [ "$lv" == "var" ]  && { f_write2fstab "$uuid" '\t/var	xfs	defaults,noatime	0 0' && continue; }
     [ "$lv" == "home" ] && { f_write2fstab "$uuid" '\t/home	xfs	defaults,noatime	0 0' && continue; }
+	    [ "$lv" == "containers" ] && { f_write2fstab "$uuid" '\t/srv/containers/\txfs\tdefaults,noatime\t0 0'; \
+      mkdir -p "/srv/containers"; continue; }
+    [ "$lv" == "volumes" ] && { f_write2fstab "$uuid" '\t/srv/volumes/\txfs\tdefaults,noatime\t0 0'; \
+      mkdir -p "/srv/volumes"; continue; }
   done
 
   echo "Перезапуск systemd"	# Пересоздаст *.mount
@@ -263,7 +276,7 @@ case $1 in
 # Подспорье: https://gist.github.com/ArcezD/c8df7b8a5735c884b927b36c6cc73006
 # TODO ЭТА ЧАСТЬ НЕ ПРОВЕРЯЛАСЬ НА РАБОТОСПОСОБНОСТЬ. Всё должно работать, но
 # не факт что будет.
-#
+
   exit 0
 
   # Пример: 6.1.0-rpi8-rpi-v8
@@ -326,6 +339,8 @@ case $1 in
 '--help'|'-help'|'help'|'-h'|*|'')	# Автопомощь. Мы тут.
   echo
   echo "ВАЖНО: почти всё берётся из преднастроек. Сначала смотрите что прописано и лишь потом запускайте."
+  echo "ТОЖЕ ВАЖНО: В скрипте полно ручников. Многие функции отключены 'во избежание'. Сначала смотрите"
+  echo "и только потом комментируйте 'exit 0'."
   echo
   echo "Недостаточно параметров или они неверно указаны."
   echo
