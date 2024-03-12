5eeb58102e75d7087d3a31e15387f2247eb03cc1
Yegor Chepurin <haegor@gmail.com>
2024-03-12 16:08:50 +0300

    Переделал болванку в полноценный скрипт

--- Organize/clean_dangling_images.sh
+++ Organize/clean_dangling_images.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 #
-# Убирает болтающиеся (осиротевшие) docker-образы
+# Подчищает всякое лишнее в docker-е.
 #
-# 2023 (c) haegor
+# 2023-2024 (c) haegor
 #
 
-docker rmi $(docker images -f "dangling=true" -q)
+# Умолчание
+[ -n "$1" ] && mode="$1" || mode='dangling'
 
-# Аналог для:
-# docker rmi $(docker image list --format "table {{.ID}}\t{{.Repository}}" | grep "<none>" | awk '{print $1}')
-
-# Более мощный вариант:
-# docker system prune -a
-#
-# А можно ещё и хранилища:
-# docker volumes prune
+case $mode in
+'dangling')		# Удалить "болтающиеся" образы
+  docker rmi $(docker images -f "dangling=true" -q)
+;;
+'axe-dangling')		# Тоже что и dangling, но более топорно
+  docker rmi $(docker image list --format "table {{.ID}}\t{{.Repository}}" | grep "<none>" | awk '{print $1}')
+;;
+'prune-all')		# Подчистить всё
+  docker system prune -a
+;;
+'volumes')		# только хранилища
+  docker volumes prune
+;;
+esac
 
